a064307713d1c1c85f5cb064ffc26631b6bf294a
Axin <axinnoemail@gmail.com>
2025-03-05 17:54:41 +0800

    用户数据初始化完成

--- nodelibrary/nodelibrary/src/com/bjpowernode/bean/User.java
+++ nodelibrary/nodelibrary/src/com/bjpowernode/bean/User.java
@@ -1,23 +1,43 @@
 package com.bjpowernode.bean;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-public class User {
-    private int id;
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    public User(int id, String name, String status, BigDecimal money) {
+    private String id;
+
+    // 姓名
+    private String name;
+
+    //状态
+    private String status;
+
+    //余额
+    private BigDecimal money;
+
+    // 是否已借书
+    private boolean isLend;
+
+    public User(String id, String name, String status, BigDecimal money) {
         this.id = id;
         this.name = name;
         this.status = status;
         this.money = money;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -45,42 +65,46 @@ public class User {
         this.money = money;
     }
 
-    private String name;
+    public boolean isLend() {
+        return isLend;
+    }
 
-    //状态
-    private String status;
-
-    //余额
-    private BigDecimal money;
+    public void setLend(boolean lend) {
+        isLend = lend;
+    }
 
     @Override
     public String toString() {
-        return "Admin{" +
+        return "User{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", status='" + status + '\'' +
                 ", money=" + money +
+                ", isLend=" + isLend +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(status, user.status) &&
-                Objects.equals(money, user.money);
+        return isLend == user.isLend && Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(status, user.status) && Objects.equals(money, user.money);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, status, money);
+        return Objects.hash(id, name, status, money, isLend);
     }
 
     public User() {
     }
 
+    public User(String name, String status, String id, BigDecimal money, boolean isLend) {
+        this.name = name;
+        this.status = status;
+        this.id = id;
+        this.money = money;
+        this.isLend = isLend;
+    }
 
 }
---
+++ nodelibrary/nodelibrary/src/com/bjpowernode/util/initDatautil.java
@@ -0,0 +1,67 @@
+package com.bjpowernode.util;
+
+import com.bjpowernode.bean.Constant;
+import com.bjpowernode.bean.User;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/*
+*  初始化数据的工具类
+* */
+public class initDatautil {
+    public static void main(String[] args) {
+        initUser();
+    }
+
+    /*
+    *  初始化用户数据
+    * */
+    public static void initUser(){
+        // 创建相关文件夹及文件
+        File durectiry = null;
+        File file = null;
+        ObjectOutputStream oos = null;
+        try{
+            durectiry = new File(Constant.USER_PATH.split("/")[0] + "/");
+            // 判断文件夹是否存在，不存在则创建
+            if (!durectiry.exists()){
+                durectiry.mkdir();
+            }
+            file = new File(Constant.USER_PATH);
+            // 判断文件是否存在，不存在则创建
+            if (!file.exists()){
+                file.createNewFile();
+                List<User> userList = new ArrayList<>();
+                // 使用 GBK 编码将字符串转换为字节数组
+                String name = "陈麻子";
+                System.out.println(name);
+                /*System.out.println(Constant.USER_OK);
+                byte[] GBKBytes = name.getBytes(Charset.forName("GBK"));
+                // 使用 GBK 编码将字节数组转换为字符串
+                name = new String(GBKBytes, Charset.forName("GBK"));*/
+                //System.out.println(name);
+                userList.add(
+                        new User(name, Constant.USER_OK,
+                                UUID.randomUUID().toString(),
+                                new BigDecimal(10), false));
+                oos = new ObjectOutputStream(new FileOutputStream(file));
+                oos.writeObject(userList);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(oos!=null){
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
